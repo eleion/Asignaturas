406bbeaaae4731a05cb42882008dce10b5782027
Luis B. <luis_ness@hotmail.com>
2014-11-18 11:40:31 +0100

    Update programa.py

--- IC/programa.py
+++ IC/programa.py
@@ -8,10 +8,10 @@
 #   a) Error de clasificación.
 #   b) Error de aproximación.
 
-# ¡¡NOTA!!: Esto sirve para una red neuronal con 8 de entrada,
-#		4 de capa oculta y 1 de salida.
-#		Hay que cambiar las cosas para una red de 3 de
-#		capa oculta.
+# ¡¡NOTA!!: Para que funcione con una .net de entrada de
+# 		distinto número de neuronas, se ha de modificar
+#		la variable 'neuronas_ocultas'.
+# 		Con eso funciona perfectamente.
 
 
 from math import exp			# Para la función e^
@@ -28,15 +28,17 @@ subprocess.call("./script.sh", shell=True)
 f_r = open('pesos_bias.txt', 'r')
 
 
-bias = []			# Este es el vector de bías.
-matriz_pesos_ocult = []		# Esta es la matriz de pesos que asocia la capa de entrada con la capa oculta.
-pesos_salida = []		# Pesos asociados a la neurona de salida.
-pesos = []			# Lista auxiliar.
-nom_fich_entrada = 'abalone.pat'# Nombre del fichero de entrada de datos.
+bias 		   = []			# Este es el vector de bías.
+matriz_pesos_ocult = []			# Esta es la matriz de pesos que asocia la capa de entrada con la capa oculta.
+pesos_salida       = []			# Pesos asociados a la neurona de salida.
+pesos 		   = [] 		# Lista auxiliar.
+neuronas_ocultas   = 5			# Número de neuronas ocultas que hay.
+nom_fich_entrada   = './pat/norm_abalone.pat'# Nombre del fichero de entrada de datos.
 
-
+# Los dos primeros no los queremos.
+f_r.readline();f_r.readline()
 # Leemos los bías (que son los 14 primeros números intercalados de 2 en 2).
-for i in range(14):
+for i in range(9 + neuronas_ocultas):
   # El primero no nos interesa
   f_r.readline()
   # El segundo sí.
@@ -44,10 +46,10 @@ for i in range(14):
   
   bias.append(float(b))
 # El primero que lee no vale. (Esto es un pequeño parche)
-bias.remove(0)
+#bias.remove(0)
 
 # Ahora vamos a por la matriz de pesos. (que son 8entradax4ocultas = 32pesos)
-for i in range(32):
+for i in range(8 * neuronas_ocultas):
   # Cambiamos las comas por puntos y quitamos el retorno de carro.
   p = f_r.readline().replace(",", ".").replace("\n", "")
   pesos.append(float(p))
@@ -58,22 +60,21 @@ for i in range(32):
     #  a menor. ¡Ojo! A mí no siempre me daba la vuelta a los 
     #  pesos. Unas veces aparece 1, 2, 3, 4, 5, 6, 7, 8; y
     #  otras veces aparece 8, 7, 6, 5, 4, 3, 2, 1. (en .net)
-    #pesos.reverse()
+    pesos.reverse()
     # Guardamos los pesos.
     matriz_pesos_ocult.append(pesos)
     pesos = []
     
 # Cogemos el resto de los pesos.
-for i in range(4):
+for i in range(neuronas_ocultas):
   p = f_r.readline().replace(",", ".").replace("\n", "")
-  print(p)
   pesos_salida.append(float(p))
 
 # Le damos la vuelta a los pesos puesto que van de mayor 
 #  a menor. ¡Ojo! A mí no siempre me daba la vuelta a los 
 #  pesos. Unas veces aparece 1, 2, 3, 4, 5, 6, 7, 8; y
 #  otras veces aparece 8, 7, 6, 5, 4, 3, 2, 1.
-#pesos_salida.reverse()
+pesos_salida.reverse()
     
 f_r.close()
 
@@ -134,14 +135,15 @@ for ent in primera_capa:
   # Cuando se está trabajando con datos NO normalizados en JavaNNS
   # le aplica directamente el bías. Esto se reduce a que a la hora
   # de implementar, no hay que meterle el bías a la capa de salida.
+  #suma = sum(mult) #+ bias[ len(bias) - 1 ]
   suma = sum(mult) #+ bias[ len(bias) - 1 ]
   
   # Le aplicamos la función sigmoidal (solo en el caso de los datos
   # noramlizados).
   #suma = 1 / (1 + exp(-suma))
     
-  # Guardamos el resultado.
-  salida.append(suma)
+  # Guardamos el resultado y discretizamos la salida.
+  salida.append(round(suma))
 
 
 
@@ -150,17 +152,32 @@ for ent in primera_capa:
 #  valor de la entrada, lo que vamos a hacer ahora es 
 #  comparar el error.
 
-MSE = 0
-for i in range(len(entrada)):
-  MSE = MSE + pow(salida[i] - entrada[i][8], 2)
-  
-MSE = MSE / len(entrada)
+#MSE = 0
+#for i in range(len(entrada)-1):
+#  try:
+#    MSE = MSE + pow(salida[i] - entrada[i][8], 2)
+#  except IndexError:
+#    print("Indice fuera de rango!!")
+#  
+#MSE = MSE / len(entrada)
 
-print("MSE:",  MSE)
+#print("MSE:",  MSE)
 
-print("Salida\t\tEntrada")
-print([salida[0], entrada[0][8]])
-print([salida[ len(salida) -1], entrada[ len(entrada) -1] [8]])
-print()
-print(bias)
-print(pesos_salida)
+# ===============================
+# Error clasificación
+# ----------------------
+e_clasif = 0
+e_aproxi = 0
+for i in range(len(entrada) - 1):
+  if salida[i] != entrada[i][8]:
+    e_clasif = e_clasif +1
+    e_aproxi = e_aproxi + (abs(salida[i] - entrada[i][8]))
+
+e_clasif = e_clasif / len(entrada)
+e_aproxi = e_aproxi / len(entrada)
+print("Error clasificación:", e_clasif)
+print("Error aproximación :", e_aproxi)
+
+
+#for i in range(5):
+#  print(entrada[i][8], "\t<->\t", salida[i])
